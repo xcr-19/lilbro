60cd8e865abd822808a50b14a9075a3167e9515f
Chandan Raj <chandanraj1919@gmail.com>
2024-06-14 16:28:13 +0200

    changed to edit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import interactions
+import asyncio
 from interactions import slash_command, SlashContext, slash_option, OptionType
 from lilb.coin_flip import CoinFlip
 from lilb.helpers import Helpers
@@ -15,14 +15,19 @@ async def on_ready():
 async def toss(ctx: SlashContext):
     coin_side = CoinFlip()
     value = coin_side.flip()
-    await ctx.send(f'Got {value}')
+    tossing_message = await ctx.send('Tossing coin 🪙', hidden=True)
+    await asyncio.sleep(1)
+    await tossing_message.edit(content=f'Got {value}')
 
 @slash_command(name='toss_times')
 @slash_option(name="times", description='Number of times to flip the coin',opt_type=OptionType.INTEGER,required=True)
 async def toss_times(ctx: SlashContext, times: int):
     coin_side = CoinFlip()
     coin_side.flip_n_times(times)
+    tossing_message = await ctx.send('Tossing coin', hidden=True)
     heads, tails = coin_side.get_results()
-    await ctx.send(f' Tossing 🪙 \n Got Heads: {heads} and Got Tails: {tails}')
+    await asyncio.sleep(1)
+    await tossing_message.edit(f'Tossed {times} times, Got Heads: {heads} and Got Tails: {tails}')
 
+bot.load_extension("interactions.ext.jurigged")
 bot.start(Helpers.get_env('DISCORD_TOKEN'))
